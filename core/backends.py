5a2738769244308729dc4df6730299c72cd55f6e
claver <82104865+cla-bit@users.noreply.github.com>
2023-12-18 07:00:54 +0100

    Create backends.py

---
+++ core/backends.py
@@ -0,0 +1,26 @@
+# from django.contrib.auth.backends import ModelBackend
+# from django.contrib.auth import get_user_model
+#
+#
+# class EmailBackend(ModelBackend):
+#     def authenticate(self, request, email=None, password=None, **kwargs):
+#         UserModel = get_user_model()
+#         try:
+#             user = UserModel.objects.get(email=email)
+#         except UserModel.DoesNotExist:
+#             return None
+#         else:
+#             if user.check_password(password):
+#                 return user
+#         return None
+#
+#     # suthenticate username
+#     def get_username(self, user):
+#         return user.username
+#
+#     def get_user(self, user_id):
+#         UserModel = get_user_model()
+#         try:
+#             return UserModel.objects.get(pk=user_id)
+#         except UserModel.DoesNotExist:
+#             return None
