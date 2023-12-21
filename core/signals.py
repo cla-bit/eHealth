04407d2232d7aa19c37b1e55481324587d5e0964
claver <82104865+cla-bit@users.noreply.github.com>
2023-12-21 13:16:13 +0100

    Update signals.py

--- core/signals.py
+++ core/signals.py
@@ -1,1 +1,23 @@
+from django.db.models.signals import post_save, post_delete, pre_save
+from django.dispatch import receiver
+from .models import CustomUser, HealthWorker, Patient, Appointment
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.conf import settings
+
+
+@receiver(post_save, sender=Appointment)
+def send_appointment_email(sender, instance, created, **kwargs):
+    if created:
+        subject = f'Appointment Request with {instance.patient.user.first_name}'
+        from_email = settings.DEFAULT_FROM_EMAIL
+        to_email = [instance.health_worker.user.email]
+        html_message = render_to_string('email/appointment_request.html', {
+            'appointment': instance
+        })
+        patient = instance.patient
+        worker = instance.worker
+        if patient and worker:
+            appointment_date = instance.date.strftime('%Y-%m-%d')
 
