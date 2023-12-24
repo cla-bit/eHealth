740d208c942c07b9bb14e66c3108a29f95d79f17
claver <82104865+cla-bit@users.noreply.github.com>
2023-12-24 22:18:44 +0100

    Update signals.py

--- core/signals.py
+++ core/signals.py
@@ -10,30 +10,14 @@ from django.conf import settings
 @receiver(post_save, sender=Appointment)
 def send_appointment_email(sender, instance, created, **kwargs):
     if created:
-        subject = f'Appointment Request with {instance.patient.user.first_name}'
-        from_email = settings.DEFAULT_FROM_EMAIL
-        to_email = [instance.health_worker.user.email]
-        html_message = render_to_string('email/appointment_request.html', {
-            'appointment': instance
-            # 'patient': instance.patient
-            # 'worker': instance.worker
-            # 'date': instance.date
-            # 'time': instance.time
-            # 'status': instance.status
-
-        })
-
+        # Send an email to the worker
         send_mail(
-            subject,
-            strip_tags(html_message),
-            from_email,
-            to_email,
-            html_message=html_message
-            # fail_silently=False
-            # html_message=html_message
+            'New Appointment Request',
+            f'Patient {instance.patient.user.username} has requested an appointment with you on {instance.date}'
+            f' at {instance.time}.\n Please accept or reject the request.\n Thank you!\n'
+            f'You can reply to {instance.patient.user.username} at {instance.patient.user.email}',
+            'from@example.com',
+            [instance.worker.user.email],
+            fail_silently=False,
         )
 
-        # patient = instance.patient
-        # worker = instance.worker
-        # if patient and worker:
-        #     appointment_date = instance.date.strftime('%Y-%m-%d')
