699201fded7a7fb4e551dd9313ab89838e92f977
claver <82104865+cla-bit@users.noreply.github.com>
2023-12-31 04:15:46 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -112,7 +112,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'core.context_processors.user_model_id',
             ],
         },
     },
