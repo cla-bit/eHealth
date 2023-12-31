2de9c9ac8e067884bcd552810f27d711c913f903
claver <82104865+cla-bit@users.noreply.github.com>
2023-12-31 04:17:46 +0100

    Update signals.py

--- core/signals.py
+++ core/signals.py
@@ -1,10 +1,39 @@
-from django.db.models.signals import post_save, post_delete, pre_save
-from django.dispatch import receiver
-from .models import CustomUser, HealthWorker, Patient, Appointment
+from django.contrib.auth.models import Group
 from django.core.mail import send_mail
-from django.template.loader import render_to_string
-from django.utils.html import strip_tags
-from django.conf import settings
+from django.db.models.signals import post_save, pre_delete, post_delete
+from django.dispatch import receiver
+from .models import CustomUser, Appointment
+from .tasks import send_email_to_new_users, send_email_to_logged_in_users, send_email_to_logged_out_users
+from allauth.account.signals import user_signed_up, user_logged_in, user_logged_out
+from .forms import CustomUserRegistrationForm
+
+
+@receiver(user_signed_up)
+def user_signed_up_handler(sender, request, user, **kwargs):
+    if user and request:
+        send_email_to_new_users.delay(user.email)
+
+
+# @receiver(post_save, sender=CustomUser)
+# def user_add_group_handler(sender, instance, created, **kwargs):
+#     if created and instance.is_worker:
+#         health_workers_group, created = Group.objects.get_or_create(name='Health Workers')
+#         instance.groups.add(health_workers_group)
+#     else:
+#         patients_group, created = Group.objects.get_or_create(name='Patients')
+#         instance.groups.add(patients_group)
+
+
+@receiver(user_logged_in)
+def user_logged_in_handler(sender, request, user, **kwargs):
+    if user and request:
+        send_email_to_logged_in_users.delay(user.id)
+
+
+@receiver(user_logged_out, sender=CustomUser)
+def user_logged_out_handler(sender, request, user, **kwargs):
+    if user and request:
+        send_email_to_logged_out_users.delay(user.email)
 
 
 @receiver(post_save, sender=Appointment)
@@ -20,4 +49,3 @@ def send_appointment_email(sender, instance, created, **kwargs):
             [instance.worker.user.email],
             fail_silently=False,
         )
-
