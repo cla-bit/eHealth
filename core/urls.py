824c0e53841a8880783e3ee5cae1b45cdf1d287c
claver <82104865+cla-bit@users.noreply.github.com>
2023-12-31 04:19:08 +0100

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from .views import (WorkerSignupView, WorkerLoginView, PatientSignupView,
-                    PatientLoginView, HomePageView, WorkerDashboardView,
-                    LogoutView, PatientDashboardView, PatientInformationView,
-                    WorkerViewPatientView, HealthStatisticView, BookAppointmentView,
-                    AcceptRejectAppointmentView
+from .views import (HomePageView, WorkerDashboardView, WorkerViewPatientView, HealthStatisticView, WorkerInformationView,
+                    PatientDashboardView, PatientInformationView, BookAppointmentView, AcceptRejectAppointmentView
                     )
 
 
@@ -11,16 +8,12 @@ app_name = 'core'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('worker/signup/', WorkerSignupView.as_view(), name='worker_signup'),
-    path('worker/login/', WorkerLoginView.as_view(), name='worker_login'),
-    path('patient/signup/', PatientSignupView.as_view(), name='patient_signup'),
-    path('patient/login/', PatientLoginView.as_view(), name='patient_login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('<int:user_id>/worker/dashboard/', WorkerDashboardView.as_view(), name='worker_dashboard'),
-    path('<int:user_id>/statistic/', HealthStatisticView.as_view(), name='health_statistic'),
-    path('<int:user_id>/patient/dashboard', PatientDashboardView.as_view(), name='patient_dashboard'),
-    path('<int:user_id>/patient/information/', PatientInformationView.as_view(), name='patient_information'),
-    path('<int:user_id>/worker/patient/detail/<int:pk>/', WorkerViewPatientView.as_view(), name='patient_detail'),
+    path('worker/<int:user_id>/dashboard/', WorkerDashboardView.as_view(), name='worker_dashboard'),
+    path('worker/<int:user_id>/patient/detail/<int:pk>/', WorkerViewPatientView.as_view(), name='patient_detail'),
+    path('worker/<int:user_id>/update/', WorkerInformationView.as_view(), name='worker_information'),
+    path('statistic/', HealthStatisticView.as_view(), name='health_statistic'),
+    path('patient/<int:user_id>/dashboard', PatientDashboardView.as_view(), name='patient_dashboard'),
+    path('patient/<int:user_id>/information/', PatientInformationView.as_view(), name='patient_information'),
     path('<int:user_id>/appointment/book/', BookAppointmentView.as_view(), name='book_appointment'),
-    path('<int:user_id>/appointment/status/', AcceptRejectAppointmentView.as_view(), name='status'),
+    path('<int:pk>/appointment/status/', AcceptRejectAppointmentView.as_view(), name='status'),
 ]
